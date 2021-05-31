caef8a4b55f3213ae6e93356c22d3177ebb3154a
evamath <84178091+evamath@users.noreply.github.com>
2021-05-31 22:23:21 +0300

    Add files via upload

---
+++ recursive fibonacci/recursive fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursive_fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give me the number for fibonacci");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+
+        }
+        public static int Fibonacci(int a)
+        {
+            if (a == 0) return 0;
+            else if (a == 1) return 1;
+            else return Fibonacci(a - 1) + Fibonacci(a - 2);
+        }
+
+    }
+}
